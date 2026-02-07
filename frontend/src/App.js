36e6369636bb17d71736649ed4b72a8ce42b626f
Vishnu122101 <vishnu.122101@gmail.com>
2026-02-08 03:17:25 +0530

    removing shit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import "@/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 const API = `${BACKEND_URL}/api`;
 
 const Home = () => {
@@ -23,14 +23,10 @@ const Home = () => {
   return (
     <div>
       <header className="App-header">
-        <a
-          className="App-link"
-          href="https://emergent.sh"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src="https://avatars.githubusercontent.com/in/1201222?s=120&u=2686cf91179bbafbc7a71bfbc43004cf9ae1acea&v=4" />
-        </a>
+        <img
+          src="https://avatars.githubusercontent.com/in/1201222?s=120&u=2686cf91179bbafbc7a71bfbc43004cf9ae1acea&v=4"
+          alt="logo"
+        />
         <p className="mt-5">Building something incredible ~!</p>
       </header>
     </div>
